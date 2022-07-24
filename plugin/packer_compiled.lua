a4fb152128ef26a9c2fc855cde3dbf9236f0638d
teemu-mjr <teemu.ma.rintala@outlook.com>
2022-07-24 20:44:10 +0300

    adds nvim-lualine/lualine.nvim

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/home/teemu-mjr/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/teemu-mjr/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
   ["nvim-tree.lua"] = {
     loaded = true,
     path = "/home/teemu-mjr/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
