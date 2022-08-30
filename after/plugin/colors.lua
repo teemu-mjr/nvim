e371d16287d4f99953f2fe8d79560c8fed19efd8
Teemu Rintala <teemu.ma.rintala@outlook.com>
2022-08-30 21:48:53 +0300

    changes and fixes the colorsheme

--- after/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
-vim.g.teemu_mjr_colorscheme = "gruvbox"
-
-function ApplyColor()
-    vim.cmd("colorscheme ".. vim.g.teemu_mjr_colorscheme)
-end
-ApplyColor()
---
+++ lua/teemu-mjr/onedark.lua
@@ -0,0 +1,12 @@
+vim.opt.background = dark
+
+vim.cmd[[silent! colorscheme onedark]]
+
+vim.api.nvim_command([[
+    augroup ChangeBackgroudColour
+        autocmd colorscheme * :hi normal termbg=#000030 termfg=#ffffff
+        autocmd colorscheme * :hi Directory ctermfg=#ffffff
+    augroup END
+]])
+
+vim.o.termguicolors = true
