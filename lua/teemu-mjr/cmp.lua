880214657215ea72dcfbf66bdd1dcda1a5ecc633
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-09-28 18:14:39 +0300

    keyword_length = 1

--- lua/teemu-mjr/cmp.lua
+++ lua/teemu-mjr/cmp.lua
@@ -13,9 +13,9 @@ cmp.setup({
   },
   sources = {
     { name = 'path' },
-    { name = 'nvim_lsp', keyword_length = 2 },
-    { name = 'buffer', keyword_length = 2 },
-    { name = 'luasnip', keyword_length = 2 },
+    { name = 'nvim_lsp', keyword_length = 1 },
+    { name = 'buffer', keyword_length = 1 },
+    { name = 'luasnip', keyword_length = 1 },
   },
   window = {
     documentation = cmp.config.window.bordered()
