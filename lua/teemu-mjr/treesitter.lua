82714d8992e6d931c1e27db9f90d5814f60df839
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-10-29 01:05:09 +0300

    fix error with .gitignore by setting auto_install=false

--- lua/teemu-mjr/treesitter.lua
+++ lua/teemu-mjr/treesitter.lua
@@ -1,31 +1,31 @@
-require("nvim-treesitter.configs").setup {
-  -- A list of parser names, or "all"
-  ensure_installed = "all",
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all"
+	ensure_installed = "all",
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = {},
+	-- List of parsers to ignore installing (for "all")
+	ignore_install = {},
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	auto_install = false,
 
-  indent = {
-    enable = true
-  },
+	indent = {
+		enable = true,
+	},
 
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
 
-    -- list of language that will be disabled
-    disable = {},
+		-- list of language that will be disabled
+		disable = {},
 
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
 
 -- Automatic folding
 -- vim.opt.foldmethod = "expr"
