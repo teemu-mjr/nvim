b634e470dbb94636dfc75596c0ab5f564b1b98e5
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-10-30 18:38:27 +0200

    remove mode from lualine

--- lua/teemu-mjr/lualine.lua
+++ lua/teemu-mjr/lualine.lua
@@ -17,10 +17,9 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_a = { "mode" },
+		lualine_a = {},
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { { "filename", path = 1 } },
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { "encoding", "fileformat" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
