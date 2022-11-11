28f6dad18d6781cf4441bb65df0b84e0a18f3797
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-11-11 23:55:51 +0200

    Small customizations to onedark theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,17 @@
 function SetOnedark()
-  vim.opt.background = "dark"
+	vim.opt.background = "dark"
+	vim.g.onedark_hide_endofbuffer = 1
+	vim.g.onedark_terminal_italics = 1
+	-- vim.o.termguicolors = true
 
-  vim.g.onedark_hide_endofbuffer = 1
-
-  vim.o.termguicolors = true
-
-  vim.g.onedark_terminal_italics = 1
-
-  vim.cmd("colorscheme onedark")
+	vim.cmd([[
+    syntax on
+    colo onedark
+    set cursorline
+    hi CursorLine ctermbg=none
+    hi CursorLineNr ctermfg=white
+    hi Search ctermfg=white ctermbg=darkgray
+    ]])
 end
 
 SetOnedark()
