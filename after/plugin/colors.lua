0048f826009abb54f19b0ea2f7c51458cb3266cb
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-11-12 18:58:38 +0200

    Edit onedark colorcheme with truecolor support

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,15 +2,20 @@ function SetOnedark()
 	vim.opt.background = "dark"
 	vim.g.onedark_hide_endofbuffer = 1
 	vim.g.onedark_terminal_italics = 1
-	-- vim.o.termguicolors = true
+	vim.o.termguicolors = true
 
-	vim.cmd([[
-    syntax on
+    vim.cmd([[
+    augroup colorset
+    autocmd!
+    let s:white = { "gui": "#FFFFFF", "cterm": "145", "cterm16" : "7" }
+    let s:gray = { "gui": "#656565", "cterm": "145", "cterm16" : "7" }
+    autocmd ColorScheme * call onedark#set_highlight("CursorLine", {})
+    autocmd ColorScheme * call onedark#set_highlight("CursorLineNr", { "fg": s:white })
+    autocmd ColorScheme * call onedark#set_highlight("Search", { "bg": s:gray, "fg": s:white })
+    augroup END
+
     colo onedark
     set cursorline
-    hi CursorLine ctermbg=none
-    hi CursorLineNr ctermfg=white
-    hi Search ctermfg=white ctermbg=darkgray
     ]])
 end
 
