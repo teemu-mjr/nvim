0963ed564dcdac183c548e063593ce74a8c05fac
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-12-25 01:57:59 +0200

    Show numbers in nvim-tree

--- lua/teemu-mjr/nvim-tree.lua
+++ lua/teemu-mjr/nvim-tree.lua
@@ -31,7 +31,7 @@ require("nvim-tree").setup({
         hide_root_folder = false,
         side = "left",
         preserve_window_proportions = false,
-        number = false,
+        number = true,
         relativenumber = true,
         signcolumn = "number",
         mappings = {
