d9d59c0d1ef75e69c4cedb2b70eac9fe55988bf6
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-12-25 17:42:54 +0200

    fix(lsp): stop starting multible instances of same lsp
    move lsp configurations inside of mason-lspconfig `setup_handlers`

--- lua/teemu-mjr/lsp/init.lua
+++ lua/teemu-mjr/lsp/init.lua
@@ -9,17 +9,8 @@ require("teemu-mjr.lsp.null-ls")
 -- enable completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-local null_ls_servers = { "tsserver", "vuels", "jsonls", "cssls" }
 
 local on_attach = function(client, bufnr)
-    -- null-ls servers
-    for _, server in pairs(null_ls_servers) do
-        if client.name == server then
-            client.server_capabilities.documentFormattingProvider = false
-            break
-        end
-    end
-
     -- hover config
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
@@ -46,27 +37,31 @@ require("mason-lspconfig").setup_handlers({
             on_attach = on_attach,
         })
     end,
-})
 
---
--- setups servers --
---
-lspconfig["sumneko_lua"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim", "use" },
+    --
+    -- setups servers --
+    --
+    ["sumneko_lua"] = function()
+        lspconfig["sumneko_lua"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "use" },
+                    },
+                },
             },
-        },
-    },
-})
+        })
+    end,
 
-lspconfig["bashls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    bash = {
-        filetypes = { "sh" },
-    },
+    ["bashls"] = function()
+        lspconfig["bashls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            bash = {
+                filetypes = { "sh" },
+            },
+        })
+    end,
 })
