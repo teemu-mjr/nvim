866bd0619a4fa9b410bc508d45f59a410a76c98c
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-02-12 12:59:39 +0200

    fix(cmp): snippet jump now work in select mode

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -36,40 +36,36 @@ cmp.setup({
             vim_item.abbr = string.sub(vim_item.abbr, 1, 20)
             vim_item.kind = string.sub(vim_item.kind, 1, 3)
             vim_item.menu = ({
-                luasnip = "[LUA]",
-                nvim_lsp = "[LSP]",
-                buffer = "[BUF]",
-            })[entry.source.name]
+                    luasnip = "[LUA]",
+                    nvim_lsp = "[LSP]",
+                    buffer = "[BUF]",
+                })[entry.source.name]
             return vim_item
         end,
     },
     mapping = {
-        ["<c-p>"] = cmp.mapping(function(fallback)
+        ["<c-p>"] = cmp.mapping(function()
             if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Item })
-            elseif luasnip.jumpable(1) then
-                luasnip.jump(1)
-            else
-                fallback()
+                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+            elseif luasnip.jumpable( -1) then
+                luasnip.jump( -1)
             end
-        end, { "i" }),
+        end, { "i", "s" }),
 
-        ["<c-n>"] = cmp.mapping(function(fallback)
+        ["<c-n>"] = cmp.mapping(function()
             if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Item })
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
             end
-        end, { "i" }),
+        end, { "i", "s" }),
 
         ["<c-j>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }, { "i" }),
 
-        ["<c-f>"] = cmp.mapping.scroll_docs(-4),
+        ["<c-f>"] = cmp.mapping.scroll_docs( -4),
         ["<c-b>"] = cmp.mapping.scroll_docs(4),
 
         ["<c-Space>"] = cmp.mapping(function()
