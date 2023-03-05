f6cb5dbe1c01a477a274b37abaf9684cb5ee3bb1
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-03-05 18:55:10 +0200

    feat(telescope): add <leader>ld for diagnostics

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,21 +1,13 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>lf", builtin.find_files)
-vim.keymap.set("n", "<leader>lg", builtin.git_files)
-vim.keymap.set("n", "<leader>la", builtin.live_grep)
-vim.keymap.set("n", "<leader>ls", function()
-    builtin.grep_string({ search = vim.fn.input("Search: ") })
-end, {})
-vim.keymap.set("n", "<leader>lb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>lh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>lm", builtin.keymaps, {})
+vim.keymap.set("n", "<leader>lf", require("telescope.builtin").find_files, {})
+vim.keymap.set("n", "<leader>lg", require("telescope.builtin").git_files, {})
+vim.keymap.set("n", "<leader>la", require("telescope.builtin").live_grep, {})
+vim.keymap.set("n", "<leader>lb", require("telescope.builtin").buffers, {})
+vim.keymap.set("n", "<leader>lh", require("telescope.builtin").help_tags, {})
+vim.keymap.set("n", "<leader>lm", require("telescope.builtin").keymaps, {})
+vim.keymap.set("n", "<leader>ld", require("telescope.builtin").diagnostics, {})
 
 require("telescope").setup({
     defaults = {
-        mappings = {
-            i = {
-                ["<c-h>"] = "select_horizontal",
-            },
-        },
         file_ignore_patterns = {
             ".git/",
         },
