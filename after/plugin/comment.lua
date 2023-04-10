6d430d853d1a141e75fe0adb673e98cbb21fc4fa
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-04-10 20:54:05 +0300

    chore(comment): remove unused settings

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,7 +1,6 @@
 require("Comment").setup({
     padding = true,
     sticky = true,
-    ignore = nil,
     toggler = {
         line = "<leader>//",
     },
@@ -19,6 +18,4 @@ require("Comment").setup({
         extra = true,
         extended = false,
     },
-    pre_hook = nil,
-    post_hook = nil,
 })
