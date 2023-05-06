71d0ce796ba75f442bba543a516358ed7911192a
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-05-06 15:32:13 +0300

    feat(telescope): add search with <leader>ls

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "<leader>lb", require("telescope.builtin").buffers, {})
 vim.keymap.set("n", "<leader>lh", require("telescope.builtin").help_tags, {})
 vim.keymap.set("n", "<leader>lm", require("telescope.builtin").keymaps, {})
 vim.keymap.set("n", "<leader>ld", require("telescope.builtin").diagnostics, {})
+vim.keymap.set("n", "<leader>ls", function()
+    require("telescope.builtin").grep_string({ search = vim.fn.input("Search: ") })
+end, {})
 
 require("telescope").setup({
     defaults = {
