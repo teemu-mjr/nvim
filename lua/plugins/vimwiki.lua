977a239ee8ee418dd6d11d64b31f552ad5720fbd
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-10-26 22:04:19 +0300

    feat(vimwiki): add home and work in md format

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,1 +1,9 @@
-return { "vimwiki/vimwiki" }
+return {
+    "vimwiki/vimwiki",
+    init = function()
+        vim.g.vimwiki_list = {
+            { path = "~/Documents/notes/home", syntax = "markdown", ext = "md" },
+            { path = "~/Documents/notes/work", syntax = "markdown", ext = "md" },
+        }
+    end
+}
