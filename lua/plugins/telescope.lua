8a924416e467dcb408312fa03a63a3cf62ccb7c3
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-11-19 00:29:54 +0200

    feat(telescope): hide hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,31 +3,17 @@ return {
     branch = "0.1.x",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    opts = {
-        defaults = {
-            file_ignore_patterns = {
-                ".git/",
-            },
-        },
-        pickers = {
-            find_files = {
-                hidden = true,
-            },
-        },
     },
     config = function()
-        vim.keymap.set("n", "<leader>lf", require("telescope.builtin").find_files, {})
-        vim.keymap.set("n", "<leader>la", require("telescope.builtin").live_grep, {})
-        vim.keymap.set("n", "<leader>lg", require("telescope.builtin").git_files, {})
-        vim.keymap.set("n", "<leader>lG", require("telescope.builtin").grep_string, {})
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>lf", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>la", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>lg", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>lG", builtin.grep_string, {})
 
-        vim.keymap.set("n", "<leader>lb", require("telescope.builtin").buffers, {})
-        vim.keymap.set("n", "<leader>lh", require("telescope.builtin").help_tags, {})
-        vim.keymap.set("n", "<leader>lm", require("telescope.builtin").keymaps, {})
-        vim.keymap.set("n", "<leader>ld", require("telescope.builtin").diagnostics, {})
-
-        require("telescope").load_extension("fzf")
+        vim.keymap.set("n", "<leader>lb", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>lh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>lm", builtin.keymaps, {})
+        vim.keymap.set("n", "<leader>ld", builtin.diagnostics, {})
     end,
 }
