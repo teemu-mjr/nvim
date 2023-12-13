007bc59cbbeb9592de161b7d1469139f527b781c
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-12-13 16:41:43 +0200

    feat(vimwiki): disable "headers" key mappings

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -5,5 +5,9 @@ return {
             { path = "~/Documents/notes/home", syntax = "markdown", ext = "md" },
             { path = "~/Documents/notes/work", syntax = "markdown", ext = "md" },
         }
+        vim.g.vimwiki_global_ext = 0
+        vim.g.vimwiki_key_mappings = {
+            headers = 0
+        }
     end
 }
