79cd615104497976f567acde37c76682d145ba1c
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-12-13 16:41:23 +0200

    feat(lazy): disable change detection

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -10,4 +10,11 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+    change_detection = {
+        enabled = false,
+    },
+    spec = {
+        { import = "plugins" }
+    }
+})
