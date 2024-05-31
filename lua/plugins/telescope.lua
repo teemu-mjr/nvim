d425470e5fa2ca20365b0d4c326f57afe64fbd7a
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2024-06-01 00:22:07 +0300

    feat(telescope): add telescope-fzf-native.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,21 @@ return {
     branch = "0.1.x",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" }
     },
     config = function()
+        require("telescope").setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                }
+            }
+        })
+        require("telescope").load_extension("fzf")
+
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>lf", builtin.find_files, {})
         vim.keymap.set("n", "<leader>la", builtin.live_grep, {})
