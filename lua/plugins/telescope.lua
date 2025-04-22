6abf8095ebadb2735dc07686e499c2c0cabcfe30
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2025-04-22 17:25:47 +0300

    feat(telescope): update fzf build command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     branch = "0.1.x",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" }
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
         require("telescope").setup({
