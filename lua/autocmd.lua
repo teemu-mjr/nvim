3d268d1df45f7d37105496dabd48320a362daa94
Teemu Rintala <teemu.rintala@triworks.dev>
2026-06-05 09:26:47 +0300

    chore(autocmd): replace `on_yank` with `hl_op`

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,6 +6,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-        vim.highlight.on_yank({ timeout = 100 })
+        vim.highlight.hl_op({ higroup = "Visual", timeout = 100 })
     end,
 })
